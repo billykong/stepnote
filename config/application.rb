22df93f277321f0fe37275e4b361d28e0b6720d8
Billy Kong <billyklh@gmail.com>
2016-12-18 23:06:23 -0800

    use doorkeeper

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,10 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+CONFIG = YAML.load(ERB.new(File.read(File.expand_path('../application.yml', __FILE__))).result(binding))
+CONFIG.merge! CONFIG.fetch(Rails.env, {})
+CONFIG.deep_symbolize_keys!
+
 module Stepnote
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
