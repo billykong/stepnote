2077638d66b05a1d73ea88c992495ce3898b7591
Billy Kong <billyklh@gmail.com>
2017-02-15 16:56:59 +0800

    spec for models

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:itinerary) { create(:itinerary) }
+  subject { create(:event, itinerary: itinerary) }
+
+  describe "When creating" do
+    it "add itself to the itinerary's events_id when created" do
+      event = create(:event, itinerary: itinerary)
+      expect(itinerary.events_id.last).to eq event.id
+    end
+  end
+
+  describe "When destroying" do
+    before do
+      subject
+    end
+
+    it "remove itself from the itinerary's events_id when destroyed" do
+      subject_id = subject.id
+      subject.destroy
+      expect(itinerary.events_id).to_not include subject_id
+    end
+  end
+
 end
--- spec/models/itinerary_spec.rb
+++ spec/models/itinerary_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Itinerary, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { create(:itinerary) }
+
+  describe "When creating" do
+    it "events_id is initialised" do
+      expect(subject.events_id).to_not be nil
+    end
+  end
 end
